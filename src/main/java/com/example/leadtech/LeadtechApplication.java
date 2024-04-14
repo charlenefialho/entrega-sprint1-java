0fe0cd6a5b00724b597d0b066132aa4a17e9f576
charlenefialho <fialhocharlene@gmail.com>
2024-04-14 20:09:51 -0300

    initial project

---
+++ src/main/java/com/example/leadtech/LeadtechApplication.java
@@ -0,0 +1,13 @@
+package com.example.leadtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadtechApplication.class, args);
+	}
+
+}
